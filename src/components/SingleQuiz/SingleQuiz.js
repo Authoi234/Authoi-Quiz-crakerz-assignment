337dd67bcd9600b7aa420f6519d09d63d4224c6f
Authoi234 <jawadkhan.authoi@gmail.com>
2023-06-25 13:32:58 +0600

    the whole website is complete

--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -5,12 +5,15 @@ import SingleQuestion from '../SignleQuestion/SingleQuestion';
 const SingleQuiz = () => {
     const quizData = useLoaderData();
     console.log(quizData);
+    const bg = {
+        backgroundColor: 'white',
+    }
     return (
         <div className=''>
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center items-center' style={bg}>
                 <img className='lg:w-48 md:w-24 sm:w-12' src={quizData.data.logo} alt="" />
             </div>
-            <div><h1 className="text-4xl">{quizData.data.name}</h1>
+            <div><h1 className="text-4xl">{quizData.data.name === 'Git' ? bg.backgroundColor= 'black' : bg.backgroundColor= 'white'} {quizData.data.name}</h1>
                 <h4 className="text-3xl">total quiz: {quizData.data.total}</h4>
                 <hr className='mt-3 mb-3 text-black' />
             </div>
